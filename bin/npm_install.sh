e2df5d537527a84352babfbe09231020c45e7a2e
Dinis Cruz <dinis.cruz@owasp.org>
2015-06-11 15:23:42 +0100

    added npm_install.sh file

---
+++ bin/npm_install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+git submodule init
+git submodule update
+
+cd code
+
+cd TM_Website
+pwd
+ls -la
+npm install
+cd
+
+pwd
--- bin/start-servers.sh
+++ bin/start-servers.sh
@@ -4,16 +4,16 @@ export TM_SITE_DATA=SiteData_TM_4
 
 echo Setting SiteData to ${siteData}
 
-echo Starting TM_4_0_Design
-cd  code/TM_4_0_Design
+echo Starting TM_Website
+cd  code/TM_Website
 #npm start &\
 node app.js &
 cd ../..
 
-echo Starting TM_4_0_GraphDB
-cd   code/TM_4_0_GraphDB
-#npm run dev &
-node index.js  &
-cd   ../..
+#echo Starting TM_4_0_GraphDB
+#cd   code/TM_4_0_GraphDB
+##npm run dev &
+#node index.js  &
+#cd   ../..
 
 wait
