728059ff358bd50347b3f04c82c022419997b119
Dinis Cruz <dinis.cruz@owasp.org>
2015-07-15 19:26:32 +0100

    Kinda fix for https://github.com/TeamMentor/TM_4_0_Design/issues/888

--- bin/reload_data.sh
+++ bin/reload_data.sh
@@ -1,12 +1,30 @@
 #!/usr/bin/env bash
 
-echo -e "\n---- reloading tm graph from Lib_Uno data ----\n"
+
+echo -e "\n---- reloading tm graph from Lib_Uno data ----\n\n"
+
+echo "...deleting Lib_UNO-json files..."
+
+
+cd data/Lib_UNO-json
+rm -R ./Articles_Html
+rm -R ./Graph_Data
+rm -R ./Mappings
+rm -R ./Search_Data
+rm -R ./Library
+cd ../..
+
+
+echo "...running tool-update-mappings.coffee..."
 
 cd build/TM_4_0_JSON_Data
 
 node ./node_modules/coffee-script/bin/coffee ./src/tool-update-mappings.coffee
 
 cd ../..
+
+echo "...copy Graph_Data..."
+cp -R data/Lib_UNO/__Graph_Data/ data/Lib_UNO-json/Graph_Data
 
 cd code/TM_GraphDB/.tmCache
 rm tm-uno-loaded.flag
