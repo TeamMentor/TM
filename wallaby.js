9e843a2b7bb722198a23d8f3e522db03f1ddd9d3
Dinis Cruz <dinis.cruz@owasp.org>
2015-07-27 19:15:58 +0100

    updated commits and improved code and data files mapped by wallaby.js

--- wallaby.js
+++ wallaby.js
@@ -5,15 +5,19 @@ module.exports = function (wallaby) {
   process.env.NODE_PATH = require('path').join(wallaby.localProjectDir, 'code', 'TM_Website', 'node_modules');
 
   //console.dir(wallaby)
-
+  console.log('[in Wallaby]');
   return {
     files: [
              'code/TM_Jade/**/*.jade',
              'code/TM_Shared/**/*.*coffee',
-             'code/TM_Static/**/*.*',
+             'code/TM_Static/**/*.css',
+             'code/TM_Static/**/*.md',
+             'code/TM_Angular/build/**/*.*',
              'code/TM_Website/src/**/*.coffee',
-             'data/Lib_Docs-json/**/*.*',
-             'config/SiteData_TM/**/*.*'
+             'data/Lib_Docs-json/Library/**/*.json',
+             'data/Lib_Docs-json/Articles_Html/00000000*.json',
+             'data/Lib_Docs-json/_Images/editingoverview-img2.jpg',
+             'config/SiteData_TM/**/*.json'
            ],
 
     tests: [
